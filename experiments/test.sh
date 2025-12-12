81332eb126a48fb7902c17ab4d812523dd673a4c
Keagan Holmes <holmes@steinbuch.ias.informatik.tu-darmstadt.de>
2025-12-12 15:33:53 +0100

    Update test file

--- experiments/test.sh
+++ experiments/test.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate onep
+
 python experiment.py \
-    --algorithm.name=uni_ppo.ppo \
+    --algorithm.name=uni_trm.ppo \
     --environment.name="talos" \
     --runner.track_console=True \
-    --runner.load_model=/home/holmes/projects/thesis/ebt/one_policy_to_run_them_all/experiments/pre_trained_model \
+    --runner.load_model=model_best_jax \
     --algorithm.determine_fastest_cpu_for_gpu=False \
     --runner.mode=test \
     --environment.mode=test \
